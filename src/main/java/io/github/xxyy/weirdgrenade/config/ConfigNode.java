821c0ef3c0db2d6f10ba23516e7080b44bca95ff
xxyy <xxyy98@gmail.com>
2014-01-25 20:59:51 +0100

    Fix default lore

--- src/main/java/io/github/xxyy/weirdgrenade/config/ConfigNode.java
+++ src/main/java/io/github/xxyy/weirdgrenade/config/ConfigNode.java
@@ -23,7 +23,7 @@ public enum ConfigNode {
     CRAFTING_OUTCOME_LORE("crafting.outcome.lore", Lists.newArrayList(
             "&7It is so weird, ",
             "&7if you throw it at the floor, it explodes.",
-            "&3You can even use" +
+            "&3You can even use",
             "&3special characters in this description:",
             "&3&auml; &szlig; &euro; &8&o(Use HTML escapes)")),
 
